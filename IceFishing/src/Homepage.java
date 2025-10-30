a0c54c7e7faa46983995be5f6430b63f9c9d8e7c
Lucasespica <lucasespicarezende@gmail.com>
2025-10-30 17:56:27 -0300

    commit att imagem

--- IceFishing/src/Homepage.java
+++ IceFishing/src/Homepage.java
@@ -5,17 +5,18 @@ import java.awt.event.*;
 public class Homepage extends JPanel {
     Image backgroundImage;
     public Homepage() {
-        ImageIcon icon = new ImageIcon("src/Sprites/Background_hd.png");
+        ImageIcon icon = new ImageIcon("src/Sprites/pixil-layer-1.png");
         backgroundImage = icon.getImage();
     }
-
+    
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
     }
+    
     public static void main(String[] args) throws Exception {
-
+        
         JFrame frame = new JFrame("Homepage");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1920, 1080);
