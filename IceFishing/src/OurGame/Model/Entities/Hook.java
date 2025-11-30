bad0caf562647aa038554bf1d2b9c624c555603b
Guilherme Eid <163929274+guilhermeeid@users.noreply.github.com>
2025-11-30 00:04:30 -0300

    Refactor Hook class by removing unused variable
    Removed unused 'y' variable and its setter method.

--- IceFishing/src/OurGame/Model/Entities/Hook.java
+++ IceFishing/src/OurGame/Model/Entities/Hook.java
@@ -3,27 +3,19 @@ package OurGame.Model.Entities;
 import OurGame.Model.Entity;
 import OurGame.Screens.Startgame;
 
-
 public class Hook extends Entity {
-    private int y;
     private Startgame game;
 
     public Hook(Startgame game, String fileName, int x, double y) {
         super(fileName, x, y);
         this.game = game;
     }
-
-    public void setY(int y) {
-        this.y = y;
-    }
     
     @Override
     public void collidedWith(Entity other) {
-        // Collision handling is done in Startgame
     }
     
     @Override
     public void closeByed(Entity other) {
-        // Not used for Hook
     }
 }
